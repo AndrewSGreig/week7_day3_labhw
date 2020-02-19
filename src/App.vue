b6118ad0f6e55094be1886e3320d4f4a06e71722
User <user@Users-MacBook-Pro.local>
2020-02-19 16:52:15 +0000

    mvp done

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,16 @@
     <div class="main-container">
       <countries-list :countries='countries'></countries-list>
       <!-- <p> {{countries}} </p> -->
-      <!-- <country-detail :country='selectedCountry'></country-detail> -->
+      <country-detail :country='selectedCountry'></country-detail>
     </div>
   </div>
 </template>
 
 <script>
 import CountriesList from './components/CountriesList.vue';
+import { eventBus } from './main.js';
+import CountryDetail from './components/CountryDetail.vue';
+
 
 export default {
   // name: 'App',
@@ -27,11 +30,17 @@ export default {
     .then(res => res.json())
     .then(countries => this.countries = countries)
 
+    eventBus.$on('country-selected', (country) => {
+  //console.log('within $on', country);
+   this.selectedCountry = country;
+});
+
 
   },
   components: {
     "countries-list": CountriesList,
-    // "country-detail": CountryDetail
+    "country-detail": CountryDetail
+
   }
 }
 </script>
---
+++ src/components/CountryDetail.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+<div v-if='country'>
+  <p> In the country detail </p>
+
+    <h3>{{ country.name}} </h3>
+    <p>Capital: {{ country.capital }} </p>
+    <p>Population: {{ country.population }} </p>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'country-detail',
+  props:['country']
+}
+</script>
+
+<style lang="css" scoped>
+  div{
+    position:fixed;
+    top: 20px;
+    right: 20px;
+  }
+</style>
